e6fb9566bc4274573c53fc8098400ab6eea8ff9c
ArthurSRods <arthur.srodrigues@hotmail.com>
2025-11-13 10:15:11 -0300

    pratica 6 inicio

---
+++ PRÁTICAS/pratica06/database.js
@@ -0,0 +1,9 @@
+import { MongoClient } from "mongodb";
+
+const url = "" ;
+const client = new MongoClient(url);
+
+export async function conectarDb() {
+  await client.connect();
+  return client.db("agenda");
+}
